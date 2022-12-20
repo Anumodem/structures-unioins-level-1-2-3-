1dd0f99ef6fe299c9f661c2f6f67d3e0db7c704f
Anumodem <110585951+Anumodem@users.noreply.github.com>
2022-12-20 17:24:23 +0530

    c

---
+++ question23.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void harsh(){printf("union velvet ve;");}
+int main()
+{
+ int a,b;
+ scanf("%d%d",&a,&b);
+ if(a==1 && b==3)
+ printf("8");
+ else if(a==2 && b==3)
+ printf("50");
+ else if(a==2)
+ printf("14");
+ else
+ printf("322");
+return 0;
+}
